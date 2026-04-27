3ad88d0482ca5d12afe2c0b7c778bb1127043ed7
Gustavo TI <gustavo@alleskaufen.com>
2026-04-27 08:19:29 -0300

    Removida restricao de rede e configurado gitignore para deploy no Render

--- electron-main.cjs
+++ electron-main.cjs
@@ -2,32 +2,7 @@ const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const os = require('os');
 
-function checkNetwork() {
-  const interfaces = os.networkInterfaces();
-  let onCorrectNetwork = false;
-
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      // Check for IPv4 and the specific subnet
-      if (iface.family === 'IPv4' && iface.address.startsWith('10.96.1.')) {
-        onCorrectNetwork = true;
-        break;
-      }
-    }
-  }
-  return onCorrectNetwork;
-}
-
 function createWindow() {
-  if (!checkNetwork()) {
-    dialog.showErrorBox(
-      'Acesso Negado',
-      'Este aplicativo só pode ser executado quando conectado à rede Wi-Fi Alles Park (10.96.1.x).'
-    );
-    app.quit();
-    return;
-  }
-
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
